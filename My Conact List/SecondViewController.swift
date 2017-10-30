dcb90005e294bb1ca517d9eb1221de25c0f81e69
Christopher Smith <christophersmith@Admins-MacBook-Air-2.local>
2017-10-30 15:32:39 -0500

    Initial Commit

---
+++ My Conact List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  My Conact List
+//
+//  Created by Christopher Smith on 10/30/17.
+//  Copyright © 2017 Christopher Smith. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
